feafe17f83cb9727c547d28b82c2a07ba19c8f8d
Leks51Ex <bulat_lesha@mail.ru>
2025-02-27 17:00:07 +0300

    prepair app settings contract

---
+++ lib/features/settings/domain/contracts/settings_repository.dart
@@ -0,0 +1,26 @@
+import 'package:flutter_boilerplate/core/failure.dart';
+import 'package:flutter_boilerplate/features/settings/domain/entities/app_settings.dart';
+import 'package:fpdart/fpdart.dart';
+
+/// Application settings repository
+///
+/// Contains methods to retrieve information of
+/// different application settings like
+/// - Theme light mode
+/// - Language
+///
+///
+
+abstract interface class SettingsRepository {
+  /// settings on current device
+  TaskEither<Failure, AppSettings> readSettings();
+
+  /// Stores application theme light mode setting
+  TaskEither<Failure, Unit> saveSettings(AppSettings settings);
+
+  /// Saves application theme settings
+  TaskEither<Failure, Unit> saveThemeSettings();
+
+  /// Saves given language
+  TaskEither<Failure, Unit> saveLanguageSettings(String languageCode);
+}
